9ec896b7d74363243d56d4a23466f0969fb50e74
AlanWehrliLC <wehrlialan1@gmail.com>
2022-10-19 23:09:53 -0300

    Component: Heading

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,26 @@
+import type { StoryObj, Meta} from "@storybook/react"
+import { Heading, HeadingProps } from "@awlc-dev/react"
+
+export default {
+    title: 'Typography/Heading',
+    component: Heading,
+    args: {
+        children: "Custom title"
+    }
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+
+export const CustomTag: StoryObj<HeadingProps> = {
+    args: {
+        children: "H1 Heading",
+        as: "h1",
+    },
+    parameters: {
+        docs: {
+            description: {
+                story: "By default, the title will always be an `h2`, but we can change it with the `as` property."
+            }
+        }
+    }
+}
